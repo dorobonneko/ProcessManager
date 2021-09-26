6af2715341a8c6dc0804018ba837704f99f9511c
dorobonneko <dorobonneko@gmail.com>
2021-09-26 11:36:56 +0800

    优化

--- app/src/main/java/com/moe/processmanager/Main.java
+++ app/src/main/java/com/moe/processmanager/Main.java
@@ -31,84 +31,96 @@ import java.io.IOException;
 
 public class Main
 {
-	public static final int UID_OBSERVER_PROCSTATE = 1<<0;
+	public static final int UID_OBSERVER_PROCSTATE = 1 << 0;
 
-    public static final int UID_OBSERVER_GONE = 1<<1;
+    public static final int UID_OBSERVER_GONE = 1 << 1;
 
-    public static final int UID_OBSERVER_IDLE = 1<<2;
+    public static final int UID_OBSERVER_IDLE = 1 << 2;
 
-    public static final int UID_OBSERVER_ACTIVE = 1<<3;
+    public static final int UID_OBSERVER_ACTIVE = 1 << 3;
 
-    public static final int UID_OBSERVER_CACHED = 1<<4;
-	
+    public static final int UID_OBSERVER_CACHED = 1 << 4;
+
 	private IActivityManager iam;
 	private IPackageManager ipm;
 	private IUsageStatsManager iusm;
 	private List<String> whiteList=new ArrayList<>();
 	private PrintStream shell;
-	public static void main(String[] args){
-		if(Os.getuid()==2000||Os.getuid()==0){
-			System.out.println("uid:"+Os.getuid());
-		Looper.prepare();
-		try{
-		new Main();
-		}catch(Throwable e){
-			e.printStackTrace();
-			System.err.println("运行出错");
-			System.exit(1);
-		}
-		System.out.println("正在后台运行");
-		Looper.loop();
+	public static void main(String[] args)
+	{
+		if (Os.getuid() == 2000 || Os.getuid() == 0)
+		{
+			System.out.println("uid:" + Os.getuid());
+			Looper.prepare();
+			try
+			{
+				new Main();
+			}
+			catch (Throwable e)
+			{
+				e.printStackTrace();
+				System.err.println("运行出错");
+				System.exit(1);
+			}
+			System.out.println("正在后台运行");
+			Looper.loop();
 		}
 		System.exit(1);
 	}
-	public Main(){
+	public Main()
+	{
 		try
 		{
 			Process p=Runtime.getRuntime().exec("sh");
-			shell=new PrintStream(p.getOutputStream());
+			shell = new PrintStream(p.getOutputStream());
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 			System.err.println("shell进程启动失败");
 		}
-		
+
+
 		IInputMethodManager imm=IInputMethodManager.Stub.asInterface(ServiceManager.getService(Context.INPUT_METHOD_SERVICE));
-		for(InputMethodInfo info:imm.getEnabledInputMethodList(0)){
+		for (InputMethodInfo info:imm.getEnabledInputMethodList(0))
+		{
 			whiteList.add(info.getPackageName());
 		}
-		iusm=IUsageStatsManager.Stub.asInterface(ServiceManager.getService(Context.USAGE_STATS_SERVICE));
+		iusm = IUsageStatsManager.Stub.asInterface(ServiceManager.getService(Context.USAGE_STATS_SERVICE));
 		IBinder binder=ServiceManager.getService("activity");
-		if(Build.VERSION.SDK_INT>26)
-			iam=ActivityManagerNative.asInterface(binder);
-			else
-		    iam=IActivityManager.Stub.asInterface(binder);
-		ipm=IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
-	    
+		if (Build.VERSION.SDK_INT > 26)
+			iam = ActivityManagerNative.asInterface(binder);
+		else
+		    iam = IActivityManager.Stub.asInterface(binder);
+		ipm = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
+
 		try
 		{
-			iam.registerUidObserver(new UidObserver(), UID_OBSERVER_CACHED|UID_OBSERVER_GONE|UID_OBSERVER_IDLE|UID_OBSERVER_ACTIVE, -1, null);
+			iam.registerUidObserver(new UidObserver(), UID_OBSERVER_CACHED | UID_OBSERVER_GONE | UID_OBSERVER_IDLE | UID_OBSERVER_ACTIVE, -1, null);
 		}
 		catch (RemoteException e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
+
 	class UidObserver extends IUidObserver.Stub
 	{
-		
+
 		@Override
 		public void onUidGone(int uid, boolean disabled) throws RemoteException
 		{
-			if(uid>10000&&uid<65535){
-				for(String packageName:ipm.getPackagesForUid(uid)){
-					if(whiteList.contains(packageName))continue;
-					ApplicationInfo ai=ipm.getApplicationInfo(packageName,PackageManager.MATCH_UNINSTALLED_PACKAGES,0);
-					if((ai.flags&ai.FLAG_SYSTEM)==0||(ai.flags&ai.FLAG_UPDATED_SYSTEM_APP)!=0){
-						iam.forceStopPackage(packageName,0);
-						System.out.println("gone:kill:"+packageName);
+			if (uid > 10000 && uid < 65535)
+			{
+				for (String packageName:ipm.getPackagesForUid(uid))
+				{
+					if (whiteList.contains(packageName))continue;
+					if(packageName.startsWith("com.google.android."))continue;
+					ApplicationInfo ai=ipm.getApplicationInfo(packageName, PackageManager.MATCH_UNINSTALLED_PACKAGES, 0);
+					if ((ai.flags & ai.FLAG_SYSTEM) == 0 || (ai.flags & ai.FLAG_UPDATED_SYSTEM_APP) != 0)
+					{
+						iam.forceStopPackage(packageName, 0);
+						System.out.println("gone:kill:" + packageName);
 					}
 				}
 			}
@@ -117,48 +129,53 @@ public class Main
 		@Override
 		public void onUidActive(int uid) throws RemoteException
 		{
-			if(uid<10000&&uid<65535){
-			//renice +0 -u u0_axxx
-			shell.println("renice +0 -u u0_a"+uid%10000);
-			shell.flush();
+			if (uid < 10000 && uid < 65535)
+			{
+				//renice +0 -u u0_axxx
+				shell.println("renice +0 -u u0_a" + uid % 10000);
+				shell.flush();
 			}
 		}
 
 		@Override
 		public void onUidIdle(int uid, boolean disabled) throws RemoteException
 		{
-			if(uid>10000&&uid<65535){
+			if (uid > 10000 && uid < 65535)
+			{
 				//renice +19 -u u0_axxx
-				shell.println("renice +8 -u u0_a"+uid%10000);
+				shell.println("renice +3 -u u0_a" + uid % 10000);
 				shell.flush();
 				//System.out.println("renice:u0_a"+uid%10000);
-				for(String packageName:ipm.getPackagesForUid(uid)){
-					iusm.setAppInactive(packageName,true,0);
+				for (String packageName:ipm.getPackagesForUid(uid))
+				{
+					iusm.setAppInactive(packageName, true, 0);
 				}
 			}
-			}
+		}
 
 		@Override
 		public void onUidStateChanged(int uid, int procState, long procStateSeq) throws RemoteException
 		{
-			
+
 		}
 
 		@Override
 		public void onUidCachedChanged(int uid, boolean cached) throws RemoteException
 		{
-			
-			if(uid>10000&&uid<65535){
+
+			if (uid > 10000 && uid < 65535)
+			{
 				//renice +19 -u u0_axxx
-				shell.println("renice +19 -u u0_a"+uid%10000);
+				shell.println("renice +10 -u u0_a" + uid % 10000);
 				shell.flush();
 				//System.out.println("renice:u0_a"+uid%10000);
-				for(String packageName:ipm.getPackagesForUid(uid)){
-					iusm.setAppInactive(packageName,true,0);
+				for (String packageName:ipm.getPackagesForUid(uid))
+				{
+					iusm.setAppInactive(packageName, true, 0);
 				}
 			}
 		}
-		
-		
+
+
 	}
 }
